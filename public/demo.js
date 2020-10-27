5aa32608798aa0c8f28c799ab8394cb722702e9a
vasil-dimov <vasil.dimov.work@gmail.com>
2020-10-27 17:04:14 +0200

    update

--- public/demo.js
+++ public/demo.js
@@ -1,19 +1,27 @@
 const status = document.getElementById('status');
-const ul = document.getElementById('installedApps');
+const ul = document.getElementById('installed-apps');
+const smartBanner = document.getElementById('smart-banner');
+const divNotSupported = document.getElementById('not-supported');
 
 window.addEventListener('load', () => {
     // Check to see if the API is supported.
     if ('getInstalledRelatedApps' in navigator) {
-        const divNotSupported = document.getElementById('notSupported');
-        divNotSupported.classList.toggle('hidden', true);
-        checkForRelatedApps();
+        var isAndroid = /(android)/i.test(navigator.userAgent);
+        if (isAndroid) {
+            smartBanner.classList.toggle('hidden', false);
+            divNotSupported.classList.toggle('hidden', true);
+            checkForRelatedApps();
+        }
     }
 });
 
 function checkForRelatedApps() {
     navigator.getInstalledRelatedApps().then((relatedApps) => {
-        status.textContent = `resolved (${relatedApps.length})`;
         relatedApps.forEach((app) => {
+            if(app.id === 'com.bg.mydemogetinstalledrelatedapps') {
+                status.textContent = `The app is installed!!!`;
+                smartBanner.classList.toggle('hidden', true);
+            }
             const lines = [];
             lines.push(`<b>id:</b> <code>${app.id}</code>`);
             lines.push(`<b>platform:</b> ${app.platform}`);
